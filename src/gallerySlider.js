665ec85206cd83dabc5f5192e0e842861c598fc8
Mario Luevanos <mario@magical-threads.com>
2020-04-17 19:15:48 -0700

    Update the gallery

--- src/gallerySlider.js
+++ src/gallerySlider.js
@@ -1,250 +1,178 @@
-export default function gallerySlider (importedImages = [], opt = {}) {
-    /*
-        Root target DIVs
-        - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-    const gallery = document.getElementById('gallery');
-    const controls = document.getElementById('controls');
-    const credits = document.getElementById('credits');
+import { EVENTS } from './events';
+import State from './state';
 
-    if (!gallery || !controls || !credits) return;
+var state = Object.assign({}, State);
+state = Object.assign(state, build());
 
-    /*
-        Gallery variables
-        - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-    var currentIndex = 0;
-    var isAnimating = false;
-    var autoPlay = null;
-    var duration = 5000;
+const duration = parseInt(getComputedStyle(state.timer).getPropertyValue('--inner-dur').replace(/s/, '')) * 1000;
+
+EVENTS.on('timerEnd', onTimerEnd);
+EVENTS.on('timerChange', onTimerChange);
+
+export default function gallerySlider () {
+    typecrop.default('.title');
+    runTimer();
+    addClickEvents();
+}
+
+function addClickEvents() {
+    Array
+        .from(document.querySelectorAll('.control-button'))
+        .forEach(button => button.addEventListener('click', onButtonClick));
     
-    /* Fire all functions needed */
-    [
-        buildFigures,
-        buildCredits,
-        buildCounter
-    ].forEach(fn => importedImages.forEach(fn));
-
-    if(opt.done) opt.done();
-    if(opt.autoPlay) autoPlay = setInterval(() => triggerMove(), duration);
-
-    /*
-        Utilities
-        - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-    function initialClass (i) {
-        return (i === 0) ? 'current' : 'next';
-    }
-
-    function classList (element) {
-        var list = element.classList;
-        return {
-            toggle: function (c) { list.toggle(c); return this; },
-            add: function (c) { list.add(c); return this; },
-            remove: function (c) { list.remove(c); return this; }
-        };
-    }
-
-    /*
-        Loop each caption
-        - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-    function buildFigureCaption (item, i) {
-        /* Captions container */
-        const figureCaption = document.createElement('figcaption');
-        const title = document.createElement('h3');
-
-        classList(title).add('title');
-
-        title.innerHTML = item.title;
-
-        /* Append created image elements to the gallery */
-        figureCaption.appendChild(title);
-        figureCaption.innerHTML += '<p>' + item.text + '</p>';
-        return figureCaption
-    }
-    /*
-        Loop each caption
-        - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-    function buildCredits (item, i) {
-        var artist = document.createElement('div');
-        artist.classList.add('credit');
-        artist.classList.add(initialClass(i));
-        artist.innerHTML += '<span>Artwork by</span><span>' + item.artist + '</span>';
-        credits.appendChild(artist);
-    }
-    /*
-        Loop each image
-        - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-    function buildFigures (item, i) {
-
-        /* Figure container and image container */
-        const figure = document.createElement('figure');
-        const image = document.createElement('div');
-        const caption = buildFigureCaption(item, i);
-
-        figure.classList.add(initialClass(i));
+    function onButtonClick(event) {
+        const {target} = event;
+        const isNext = target.classList.contains('next');
+        const isPrev = target.classList.contains('prev');
         
-        image.classList.add('image');
-        image.style.backgroundImage = `url(${ item.background })`;
-
-        figure.appendChild(image);
-        figure.appendChild(caption);
-        gallery.appendChild(figure);
+        if (isNext) onTimerEnd();
+        if (isPrev) onTimerEnd(false);
     }
-    /*
-        Slider counter for UX purposes
-        - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-    function buildCounter (item, i) {
+}
 
-        item = null;
-
-        if(i === 0) {
-            /* Image counter */
-            var counterE = document.createElement('div');
-            var counterTotal = document.createElement('span');
-            var counterIndex = document.createElement('span');
-            var currentSlide = currentIndex + 1;
-
-            /* And total number of slides */
-            counterTotal.innerHTML = (importedImages.length);
-            counterIndex.innerHTML = currentSlide;
-
-            // Add image counter */
-            classList(counterE)
-                .add('counter');
-            classList(counterIndex)
-                .add('counter-index');
-
-            counterE
-                .appendChild(counterIndex);
-            counterE
-                .appendChild(counterTotal);
-            controls
-                .appendChild(counterE);
-        }
-        else {
-            return false;
-        }
+function onTimerEnd(isNext = true) {
+    const prev = state.currentIndex;
+    if (isNext) {
+        const curr = prev === state.gallery.length - 1 ? 0 : prev + 1;
+        const next = curr === state.gallery.length - 1 ? 0 : curr + 1;
+        EVENTS.emit('timerChange', { prev, curr, next });
+        console.log('isNext', { prev, curr, next });
     }
-
-    /*
-        Stylistic option for titles
-        - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-    function updateCounterIndex () {
-        controls.querySelector('.counter-index').innerHTML = currentIndex + 1;
+    else {
+        const curr = prev === 0 ? state.gallery.length - 1 : prev - 1;
+        const next = curr === 0 ? state.gallery.length - 1 : curr - 1;
+        console.log('isPrev', { prev, curr, next });
+        EVENTS.emit('timerChange', { prev, curr, next });
+        
     }
+}
 
-    function triggerMove (event) {
+function onTimerChange(event) {
+    const { curr } = event;
+    updateClassNames('figures', event);
+    updateClassNames('credits', event);
+    state.currentIndex = curr;
+    state.counter.innerHTML = (curr + 1); // non-zero
+}
 
-        var dirNext, dirPrev, direction;
+function updateClassNames(elementName, event) {
+    const { prev, curr, next } = event;
+    state[elementName][prev].className = 'prev';
+    state[elementName][curr].className = 'current';
+    state[elementName][next].className = 'next';
+}
 
-        if(event) {
-            clearInterval(autoPlay);
-            /* Use regex to test if clicked has class of next or prev */
-            var testNext = /\b(next)\b/g;
-            var testPrev = /\b(prev)\b/g;
-            dirNext = testNext.test(event.target.className);
-            dirPrev = testPrev.test(event.target.className);
-        } else {
-            dirNext = true;
-        }
+function runTimer() {
+    setTimeout(() => state.timer.classList.add('active'), duration);
+}
 
-        if(isAnimating) { return; }
-
-        isAnimating = true;
-
-        if(dirNext) {
-            /* If at the end of the array */
-            if(currentIndex === importedImages.length - 1) {
-                /* Reset back to first */
-                currentIndex = 0;
-                direction = 'begin';
-            } else {
-                currentIndex++;
-                direction = 'next';
-            }
-        }
-
-        if(dirPrev) {
-            /* If at the begining of the array going in reverse */
-            if(currentIndex === 0) {
-                /* Set back to last item in array */
-                currentIndex = importedImages.length - 1;
-                direction = 'end';
-            } else {
-                currentIndex--;
-                direction = 'prev';
-            }
-        }
-        /*
-            Pass in the gallery items and captions
-            to the move() for animating with CSS control
-        */
-        move([
-            gallery.children,
-            credits.children
-        ], direction);
+function onTimerTransitionEnd(event) {
+    const timer = event.target;
+    if(event.propertyName === 'left') {
+        EVENTS.emit('timerEnd');
     }
-
-    function move (items, direction) {
-
-        items.forEach(function (item) {
-            var dir
-                = (direction === 'next') ? 'prev'
-                    : (direction === 'prev') ? 'next'
-                        : 'current';
-
-            var index
-                = (direction === 'next') ? (currentIndex - 1)
-                    : (direction === 'prev') ? (currentIndex + 1)
-                        : (direction === 'begin') ? 0
-                            : (direction === 'end') ? importedImages.length - 1
-                                : 0;
-
-            classList(item[currentIndex])
-                .remove(direction)
-                .add('current');
-
-            classList(item[index])
-                .remove('current')
-                .add(dir);
-
-            /* Make slider reset to cycle infinitely */
-            if(direction === 'begin' || direction === 'end') {
-                [].slice.call(item).forEach(function (val) {
-                    classList(val)
-                        .remove(direction === 'begin' ? 'prev' : 'next')
-                        .add(direction === 'begin' ? 'next' : 'prev')
-                        .remove('current');
-                });
-                classList(item[index])
-                    .remove('next')
-                    .remove('prev')
-                    .add('current');
-            }
-
-            animationGate(item);
-
-        });
-
-        updateCounterIndex();
+    if(timer.classList.contains('active') && timer.classList.contains('exit')) {
+        timer.classList.remove('active');
+        timer.classList.remove('exit');
+    } else {
+        timer.classList.add('exit');
     }
+    // Restart the timer
+    runTimer();
+}
 
-    function animationGate (item) {
-        var elapsedTime;
-        /* Cycle through the elements to check if animation is complete */
-        [].slice.call(item).forEach(function (elements) {
-            elements.addEventListener('transitionend', (e) => {
-                elapsedTime = e.elapsedTime;
-                setTimeout(() => {
-                    /* Change animating flag */
-                    if(e.elapsedTime < elapsedTime) {
-                        return;
-                    }
-                    else {
-                        isAnimating = false;
-                    }
-                }, 0);
-            });
-        });
-    }
+function onFigureTransitionEnd(event) {
+    console.log(event);
+}
 
-    controls.addEventListener('click', triggerMove, false);
+// ------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------
+
+function build () {
+    const { gallery } = state;
+    const elements = [buildFigures, buildCredits].map(fn => gallery.map(fn.bind(null, gallery)));
+    const [figures, credits] = elements;
+    const counter = buildCounter(gallery);
+    const timer = buildTimer();
+    return {
+        timer,
+        counter,
+        figures,
+        credits
+    };
+}
+
+function buildTimer () {
+    const slider = document.getElementById('slider');
+    const timer = document.createElement('div');
+    slider.appendChild(timer);
+    timer.classList.add('timer');
+    timer.addEventListener('transitionend', onTimerTransitionEnd);
+    return timer;
+}
+
+function buildFigureCaption (item) {
+    
+    /* Captions container */
+    const figureCaption = document.createElement('figcaption');
+    const title = document.createElement('h3');
+
+    title.classList.add('title');
+    title.innerHTML = item.title;
+
+    /* Append created image elements to the gallery */
+    figureCaption.appendChild(title);
+    figureCaption.innerHTML += '<p>' + item.text + '</p>';
+    return figureCaption;
+}
+
+function buildCredits (items, item, idx) {
+    const credits = document.getElementById('credits');
+    const artist = document.createElement('div');
+    artist.className = idx === 0 ? 'current' : 'next';
+    artist.innerHTML += '<span>Artwork by</span><span>' + item.artist + '</span>';
+    credits.appendChild(artist);
+    return artist;
+}
+
+function buildFigures (images, item, i) {
+    /* Figure container and image container */
+    const gallery = document.getElementById('gallery');
+    const figure = document.createElement('figure');
+    const image = document.createElement('div');
+    const caption = buildFigureCaption(item);
+
+    image.classList.add('image');
+    image.style.backgroundImage = `url(${ item.background })`;
+    figure.className = i === 0 ? 'current' : 'next';
+    figure.appendChild(image);
+    figure.appendChild(caption);
+    gallery.appendChild(figure);
+
+    figure.addEventListener('transitionend', onFigureTransitionEnd);
+
+    return figure;
+}
+
+function buildCounter (images) {
+    const controls = document.getElementById('controls');
+    
+    /* Image counter */
+    const counterE = document.createElement('div');
+    const counterTotal = document.createElement('span');
+    const counterIndex = document.createElement('span');
+
+    /* And total number of slides */
+    counterTotal.innerHTML = (images.length);
+    counterIndex.innerHTML = state.currentIndex + 1;
+
+    // Add image counter */
+    counterE.classList.add('counter');
+    counterIndex.classList.add('counter-index');
+
+    counterE.appendChild(counterIndex);
+    counterE.appendChild(counterTotal);
+    controls.appendChild(counterE);
+    return counterIndex;
 }
--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
 import gallerySlider from './gallerySlider'
-import TypeTest from './TypeTest'
+// import TypeTest from './TypeTest'
 import allAlphabet from './allAlphabet'
 const typeCrop = typecrop.default
 
-gallerySlider([
-    {
-        title: 'DeviantArt Typography',
-        text: 'SVG replacement for the web.',
-        artist: 'DanielaUhlig',
-        background: require('/assets/images/img-1.jpg')
-    },
-    {
-        title: 'Audacious and Inspired',
-        text: 'We have a unique and ownable headline typography that highlights our audacious and inspired characters.',
-        artist: 'Mark-Chadwick',
-        background: require('/assets/images/img-2.jpg')
-    },
-    {
-        title: 'Sixty Two Degree Angle',
-        text: 'The first and last letter of the headline are customized at our 62 degree angle, helping each communication tell our story.',
-        artist: 'Aymen-Ouertani',
-        background: require('/assets/images/img-3.jpg')
-    },
-    {
-        title: 'Examples and Usage',
-        text: 'Below you see how the full alphabet has been crafted, a demonstration and examples of usage.',
-        artist: 'axcy',
-        background: require('/assets/images/img-4.jpg')
-    }
-], {
-    autoPlay: true,
-    done: () => typeCrop('.title')
-})
+gallerySlider();
 
-new TypeTest()
+// {
+//     autoPlay: true,
+//     done: () => typeCrop('.title')
+// }
+
+// new TypeTest()
 
 allAlphabet('#all-alphabet')
 
